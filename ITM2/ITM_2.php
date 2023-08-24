46dd081968400bab8333db618b6c7286a814e755
CodefromMax <mr.wang@mail.utoronto.ca>
2023-08-23 23:48:55 -0400

    Add Update.

--- ITM2/ITM_2.php
+++ ITM2/ITM_2.php
@@ -2,20 +2,25 @@
 <!-- <html>  
     <head>   -->
         <!-- <title>Webslesson Demo - Live Table Add Edit Delete using Ajax Jquery in PHP Mysql</title>   -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />   -->
         <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>   -->
         <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>   -->
         <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> -->
     <!-- </head>  
-    <body>   -->
+    <body>   <form name = "add_item" action ="" method = "post" >
+        <input type = "text" id = "txtnameis" placeholder="Item Name">
+</form>-->
+
+        
         <div id = "disp_data"></div>
 
         <script>
             function disp_data()
             {
                 var xmlhttp = new XMLHttpRequest();
-                xmlhttp.open("GET", "update.php?status=disp",false);
-                xmlhttp.send(null);
+                xmlhttp.open("POST", "update.php",false);
+                xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xmlhttp.send("Action=disp");
                 document.getElementById("disp_data").innerHTML=xmlhttp.responseText;
             }
             disp_data()
@@ -45,10 +50,10 @@
             // }
             // else{
 
-                nameid = Column+id;
-                txtnameid = "txt"+Column+id;
-                var name =document.getElementById(nameid).innerHTML;
-                document.getElementById(nameid).innerHTML="<input type = 'text' value='"+name+"' id ='"+txtnameid+"' size = '9'>";
+                replace_id = Column+id;
+                txt_replace_id = "txt"+Column+id;
+                var replace_input =document.getElementById(replace_id).innerHTML;
+                document.getElementById(replace_id).innerHTML="<input type = 'text' value='"+replace_input+"' id ='"+txt_replace_id+"' size = '9'>";
             // }
             
         }
@@ -77,6 +82,100 @@
 
         }
 
+        function update1(id)
+        {
+            var Name_id = "txtName"+id;
+            var Name = document.getElementById(Name_id).value;
+
+            var Supplier_id = "txtSupplier"+id;
+            var Supplier = document.getElementById(Supplier_id).value;
+
+            var Est_Quantity_id = "txtEst_Quantity"+id;
+            var Est_Quantity = document.getElementById(Est_Quantity_id).value;
+
+            var Exact_Quantity_id = "txtExact_Quantity"+id;
+            var Exact_Quantity = document.getElementById(Exact_Quantity_id).value;
+
+            var Minimum_id = "txtMinimum"+id;
+            var Minimum = document.getElementById(Minimum_id).value;
+
+            var Boxes_id = "txtBoxes"+id;
+            var Boxes = document.getElementById(Boxes_id).value;
+
+            var Owner_Name_id = "txtOwner_Name"+id;
+            var Owner_Name = document.getElementById(Owner_Name_id).value;
+
+            var Status_id = "txtStatus"+id;
+            var Status = document.getElementById(Status_id).value;
+
+            var Room_id = "txtRoom"+id;
+            var Room = document.getElementById(Room_id).value;
+
+            var Section_id = "txtSection"+id;
+            var Section = document.getElementById(Section_id).value;
+
+            var Shelf_id = "txtShelf"+id;
+            var Shelf = document.getElementById(Shelf_id).value;
+
+            var Level_id = "txtLevel"+id;
+            var Level = document.getElementById(Level_id).value;
+
+            var Note_id = "txtNote"+id;
+            var Note =document.getElementById(Note_id).value;
+
+            // update_data(id, Name);
+            update_data(id, Name, Supplier, Est_Quantity, Exact_Quantity, Minimum, Boxes, Owner_Name, Status, Room, Section, Shelf, Level, Note );
+
+            document.getElementById("Name"+id).innerHTML= Name;
+
+            document.getElementById("Supplier"+id).innerHTML= Supplier;
+
+            document.getElementById("Est_Quantity"+id).innerHTML= Est_Quantity;
+
+            document.getElementById("Exact_Quantity"+id).innerHTML= Exact_Quantity;
+
+            document.getElementById("Minimum"+id).innerHTML= Minimum;
+
+            document.getElementById("Boxes"+id).innerHTML= Boxes;
+
+            document.getElementById("Owner_Name"+id).innerHTML= Owner_Name;
+        
+            document.getElementById("Status"+id).innerHTML= Status;
+
+            document.getElementById("Room"+id).innerHTML= Room;
+
+            document.getElementById("Section"+id).innerHTML= Section;
+
+            document.getElementById("Shelf"+id).innerHTML= Shelf;
+
+            document.getElementById("Level"+id).innerHTML = Level;
+
+            document.getElementById("Note"+id).innerHTML = Note;
+        }
+
+        function update_data(id, Name, Supplier, Est_Quantity, Exact_Quantity, Minimum, Boxes, Owner_Name, Status, Room, Section, Shelf, Level, Note ){
+            
+            // function update_data(id, Name){
+            // xmlhttp = new XMLHttpRequest();
+            // xmlhttp.open("GET","update.php?id="+id+"&name="+Name+"&Supplier="+Supplier+"&Est_Quantity="+Est_Quantity+"&Exact_Quantity="+Exact_Quantity+"&Boxes="+Boxes+"&Owner_Name="+Owner_Name+"&Status="+Status+"&Room="+Room+"&Section="+Section+"&Shelf="+Shelf+"&Level="+Level+"&Note="+Note+"&Action=Update")
+            
+            xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("POST", "update.php", false);
+            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+            variable = "id="+id+"&Name="+Name+"&Supplier="+Supplier+"&Est_Quantity="+Est_Quantity+"&Exact_Quantity="+Exact_Quantity+"&Minimum="+Minimum+"&Boxes="+Boxes+"&Owner_Name="+Owner_Name+"&Status="+Status+"&Room="+Room+"&Section="+Section+"&Shelf="+Shelf+"&Level="+Level+"&Note="+Note+"&Action=Update";
+            
+            xmlhttp.send(variable);
+
+        }
+
+        function delete1(id){
+            xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("POST", "update.php", false);
+            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+            variable = "id="+id+"&Action=Delete";
+            xmlhttp.send(variable);
+            disp_data();
+        }
 
         </script>
             
--- ITM2/update.php
+++ ITM2/update.php
@@ -1,8 +1,8 @@
 <?php  
  
- $status = $_GET["status"];
+ $Action = $_POST["Action"];
 
- if ($status == "disp"){
+ if ($Action == "disp"){
 
      $connect = mysqli_connect("localhost", "root", "", "inv");  
      $output = '';  
@@ -32,16 +32,27 @@
                     <th style = "text-align: center">Delete</th>
                 </tr>';  
      echo $output;
-     // echo "<table>"; style="width:90%" align =  "center"
-     //  <td class="Section" data-id9="'.$row["Section"].'-'.$row["Shelf"].'-'.$row["Level"].'"contenteditable>'.$row["Section"].'-'.$row["Shelf"].'-'.$row["Level"].'</td>  
+?>
+     <tr>  
+     <td></td>  
+               <td id="Item_Name" contenteditable></td>
+               <td id="Supplier" contenteditable></td>
+               <td id="Est_Quantity" contenteditable></td> 
+               <td id="Exact_Quantity" contenteditable></td> 
+               <td id="Boxes" contenteditable></td> 
+               <td id="Owner_Name" contenteditable></td> 
+               <td id="Status" contenteditable></td> 
+               <td id="Room" contenteditable></td> 
+               <td id="Section" contenteditable></td> 
+               <td id="Shelf" contenteditable></td> 
+               <td id="Level" contenteditable></td> 
+               <td id="Note" contenteditable></td> 
+          <td><button type="button" name="btn_add" id="btn_add" class="btn btn-xs btn-success">+</button></td>  
+     </tr>  
+     <?php
      if($rows > 0)  
      {  
-          //   if($rows > 10)
-          //   {
-          // 	  $delete_records = $rows - 10;
-          // 	  $delete_sql = "DELETE FROM ITM_inventory LIMIT $delete_records";
-          // 	  mysqli_query($connect, $delete_sql);
-          //   }
+          
           while($row = mysqli_fetch_array($result))  
           {  
                
@@ -62,144 +73,55 @@
                    echo "<td>";?> <div id = "Note<?php echo $row["Item_ID"]; ?>"><?php echo $row["Note"]; ?></div> <?php echo "</td>";  
                    echo "<td>";?>
                     <input type="button" name="<?php echo $row["Item_ID"]; ?>" id="<?php echo $row["Item_ID"]; ?>" class="btn btn-primary btn-block" value = "Edit" onclick="edit1(this.id)">
-                    <input type="button" name="<?php echo $row["Item_ID"];?>" id="update<?php echo $row["Item_ID"]; ?>" value = "Update" class="btn btn-primary btn-block" onclick="update1(this.id)" style = "display:none" ><?php echo "</td>"; 
+                    <input type="button" name="<?php echo $row["Item_ID"];?>" id="update<?php echo $row["Item_ID"]; ?>" value = "Update" class="btn btn-primary btn-block" onclick="update1(this.name)" style = "display:none" ><?php echo "</td>"; 
                    echo "<td>";?> <input type="button" name="<?php echo $row["Item_ID"]; ?>" id="<?php echo $row["Item_ID"]; ?>" class="btn btn-danger btn_delete" value = "Delete" onclick="delete1(this.id)"><?php echo "</td>"; 
                    echo "</tr>"; 
           }
      }
+
+
+
      echo "</table>";
      // echo "</div>";
 
 }
- 
- 
-//  $connect = mysqli_connect("localhost", "root", "", "inv");  
-//  $output = '';  
-//  $sql = "SELECT * FROM ITM_inventory ORDER BY Item_ID ASC";  
-//  $result = mysqli_query($connect, $sql);  
-//  $output .= '  
-//       <div class="table-responsive">  
-//            <table class="table table-bordered">  
-//                 <tr>  
-// <th width="5%">Id</th>  
-//                      <th width="30%">Name</th>  
-//                      <th width="10%">Supplier</th>  
-//                      <th width="3%">Est. Quantity</th>  
-//                      <th width="3%">Exact Quantity</th>  
-//                      <th width="3%">Minimum</th>  
-//                      <th width="5%">No. Boxes</th>  
-//                      <th width="10%">Owner</th>  
-//                      <th width="10%">Status</th>  
-//                      <th width="5%">Room</th>  
-//                      <th width="2%">Section</th>  
-//                      <th width="2%">Shelf</th>  
-//                      <th width="2%">Level</th>  
-//                      <th width="10%">Note</th>
-//                      <th width="10%">Delete</th>
-//                      <th width="5%">Id</th>  
-//                      <th width="30%">Name</th>  
-//                      <th width="10%">Supplier</th>  
-//                      <th width="5%">Est. Quantity</th>  
-//                      <th width="5%">Exact Quantity</th>  
-//                      <th width="5%">No. Boxes</th>  
-//                      <th width="10%">Owner</th>  
-//                      <th width="10%">Status</th>  
-//                      <th width="5%">Room</th>  
-//                      <th width="2%">Section</th>  
-//                      <th width="2%">Shelf</th>  
-//                      <th width="2%">Level</th>  
-//                      <th width="10%">Note</th>
-//                      <th width="10%">Delete</th>
-//                 </tr>';  
-//  $rows = mysqli_num_rows($result);
 
-// //  <td class="Section" data-id9="'.$row["Section"].'-'.$row["Shelf"].'-'.$row["Level"].'"contenteditable>'.$row["Section"].'-'.$row["Shelf"].'-'.$row["Level"].'</td>  
-//  if($rows > 0)  
-//  {  
-// 	//   if($rows > 10)
-// 	//   {
-// 	// 	  $delete_records = $rows - 10;
-// 	// 	  $delete_sql = "DELETE FROM ITM_inventory LIMIT $delete_records";
-// 	// 	  mysqli_query($connect, $delete_sql);
-// 	//   }
-//       while($row = mysqli_fetch_array($result))  
-//       {  
-//            $output .= '  
-//                 <tr>  
-//                      <td class="Item_ID" data-id0 = "'.$row["Item_ID"].'">'.$row["Item_ID"].'</td>  
-//                      <td class="Item_Name" data-id1="'.$row["Item_ID"].'" contenteditable>'.$row["Item_Name"].'</td>  
-//                      <td class="Supplier" data-id2="'.$row["Item_ID"].'" contenteditable>'.$row["Supplier"].'</td>  
-//                      <td class="Est_Quantity" data-id3="'.$row["Item_ID"].'" contenteditable>'.$row["Est_Quantity"].'</td>  
-//                      <td class="Exact_Quantity" data-id4="'.$row["Item_ID"].'" contenteditable>'.$row["Exact_Quantity"].'</td>  
-//                      <td class="Boxes" data-id5="'.$row["Item_ID"].'" contenteditable>'.$row["Boxes"].'</td>  
-//                      <td class="Owner_Name" data-id6="'.$row["Item_ID"].'" contenteditable>'.$row["Owner_Name"].'</td>  
-//                      <td class="Status" data-id7="'.$row["Item_ID"].'" contenteditable>'.$row["Status"].'</td>  
-//                      <td class="Room" data-id8="'.$row["Item_ID"].'" contenteditable>'.$row["Room"].'</td>  
-//                      <td class="Section" data-id9="'.$row["Item_ID"].'"contenteditable>'.$row["Section"].'</td>  
-//                      <td class="Shelf" data-id10="'.$row["Item_ID"].'"contenteditable>'.$row["Shelf"].'</td>  
-//                      <td class="Level" data-id11="'.$row["Item_ID"].'"contenteditable>'.$row["Level"].'</td>  
-//                      <td class="Note" data-id12="'.$row["Item_ID"].'"contenteditable>'.$row["Note"].'</td>  
-//                      <td><button type="button" name="delete_btn" data-id13="'.$row["Item_ID"].'" class="btn btn-xs btn-danger btn_delete">x</button></td>  
-//                 </tr>  
-//            ';  
-//       }  
-//       $output .= '  
-//            <tr>  
-//            <td></td>  
-//                      <td id="Item_Name" contenteditable></td>
-//                      <td id="Supplier" contenteditable></td>
-//                      <td id="Est_Quantity" contenteditable></td> 
-//                      <td id="Exact_Quantity" contenteditable></td> 
-//                      <td id="Boxes" contenteditable></td> 
-//                      <td id="Owner_Name" contenteditable></td> 
-//                      <td id="Status" contenteditable></td> 
-//                      <td id="Room" contenteditable></td> 
-//                      <td id="Section" contenteditable></td> 
-//                      <td id="Shelf" contenteditable></td> 
-//                      <td id="Level" contenteditable></td> 
-//                      <td id="Note" contenteditable></td> 
-//                 <td><button type="button" name="btn_add" id="btn_add" class="btn btn-xs btn-success">+</button></td>  
-//            </tr>  
-//       ';  
-//  }  
-//  else  
-//  {  
-//       $output .= '
-// 				<tr>  
-// 					<td></td>  
-// 					<td id="Item_Name" contenteditable></td>
-//                          <td id="Supplier" contenteditable></td>
-//                          <td id="Est_Quantity" contenteditable></td> 
-//                          <td id="Exact_Quantity" contenteditable></td> 
-//                          <td id="Boxes" contenteditable></td> 
-//                          <td id="Owner_Name" contenteditable></td> 
-//                          <td id="Status" contenteditable></td> 
-//                          <td id="Room" contenteditable></td> 
-//                          <td id="Section" contenteditable></td> 
-//                          <td id="Shelf" contenteditable></td> 
-//                          <td id="Level" contenteditable></td> 
-//                          <td id="Note" contenteditable></td> 
-// 					<td><button type="button" name="btn_add" id="btn_add" class="btn btn-xs btn-success">+</button></td>  
-// 			   </tr>';  
-//  }  
-//  $output .= '</table>  
-//       </div>';  
-//  echo $output;  
+if ($Action == "Update")
+{
+     $connect = mysqli_connect("localhost", "root", "", "inv");
+	
+     $id = $_POST["id"];
+     $Name = $_POST["Name"];
+     $Supplier= $_POST["Supplier"];
+     $Est_Quantity= $_POST["Est_Quantity"];
+     $Exact_Quantity= $_POST["Exact_Quantity"];
+     $Minimum = $_POST["Minimum"];
+     $Boxes= $_POST["Boxes"];
+     $Owner_Name= $_POST["Owner_Name"];
+     $Status= $_POST["Status"];
+     $Room= $_POST["Room"];
+     $Section= $_POST["Section"];
+     $Shelf= $_POST["Shelf"];
+     $Level= $_POST["Level"];
+     $Note= $_POST["Note"];
+     
+     $query = "UPDATE `ITM_Inventory` SET `Item_Name`='$Name',`Supplier`='$Supplier',`Est_Quantity`='$Est_Quantity',`Exact_Quantity`='$Exact_Quantity',`Minimum`='$Minimum',`Boxes`='$Boxes',`Owner_Name`='$Owner_Name',`Status`='$Status',`Room`='$Room',`Section`='$Section',`Shelf`='$Shelf',`Level`='$Level',`Note`='$Note' WHERE `Item_ID` = '$id' ";
+     // $query = "UPDATE `ITM_Inventory` SET `Item_Name`='$Name', `Supplier`='$Supplier',`Est_Quantity`='$Est_Quantity',`Exact_Quantity`='$Exact_Quantity' WHERE `Item_ID` = '$id' ";
+     // ,`Supplier`='$Supplier',`Est_Quantity`='$Est_Quantity',`Exact_Quantity`='$Exact_Quantity',`Minimum`='$Minimum',`Boxes`='$Boxes',`Owner_Name`='$Owner_Name',`Status`='$Status',`Room`='$Room',`Section`='$Section',`Shelf`='$Shelf',`Level`='$Level',`Note`='$Note' WHERE `Item_ID` = '$id' ";
 
-//  Note:
-// data-id1
-// Above Jquery code is write for Live Update or Edit of table column first_name, 
-// We have write JQuery code on <td> tag with class selector on blur event, 
-// So when we have leave first_name <td> tag then this code will execute. 
-// Under this function it will received id from data-id1 attribute and text 
-// of first name get from <td> class attribute with text() method. 
-// This method will fetch text from <td> tag and store into one variable. 
-// Then after we have called edit_data() function which send Ajax request to edit.php
-//  page and then after it will update or edit first_name table column data.
+     mysqli_query($connect, $query);
 
-     //  $(document).on('blur', '.last_name', function(){  
-     //       var id = $(this).data("id2");  
-     //       var last_name = $(this).text();  
-     //       edit_data(id,last_name, "last_name");  
-     //  });
- ?>
+}
+
+if ($Action == "Delete")
+{
+     $connect = mysqli_connect("localhost", "root", "", "inv");
+	
+     $id = $_POST["id"];
+     $query = "DELETE FROM `ITM_Inventory` WHERE `Item_ID` = '$id'";
+     mysqli_query($connect, $query);
+}
+
+
+
+?>
